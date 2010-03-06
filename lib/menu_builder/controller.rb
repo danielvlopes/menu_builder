40f0915758be1a5efe6e91903b92c13a36d03ec5
Daniel Lopes <danielvlopes@gmail.com>
2010-03-06 14:28:04 -0300

    removing whitespaces

--- lib/menu_builder/controller.rb
+++ lib/menu_builder/controller.rb
@@ -1,11 +1,11 @@
 module MenuBuilder
   module Controller
-    
+
     def current_tab(name, options = {})
       before_filter(options) do |controller|
         controller.instance_variable_set('@current_tab', name)
       end
     end
-    
+
   end
 end
